43b7ab47d27528b8e64840189429867caa86750e
Sergio <ruizdiaz_sergio@outlook.com>
2025-08-27 17:02:22 -0300

    links corregidos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,53 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSackDollar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSackDollar } from "@fortawesome/free-solid-svg-icons";
 
-import '../styles/Header.css'
+import "../styles/Header.css";
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
-    return (
-        <header className='holder'>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-                integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-                crossorigin="anonymous" referrerpolicy="no-referrer" />
-            <link href="https://fonts.googleapis.com/css2?family=Agbalumo&family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet"></link>
-            <div className="descuento">
-                <p>¡Envío gratis en compras a partir de $15.000!</p>
-                <FontAwesomeIcon icon={faSackDollar} bounce size="2xl" style={{ color: "#16ca43", }} />
-            </div>
-            <div className="boton">
-                <button type="submit" className="btn btn-outline-danger btn-lg">
-                    <a href="http://localhost:3000/admin/novedades" target="_blank" rel='noopener noreferrer'>
-                        <h3>Ingresar  <i class="fa-solid fa-right-to-bracket"></i></h3>
-                    </a>
-                </button>
-            </div>
-            <div className="logo">
-                <img src="img/logo.png" alt="logo" />
-                <h1>
-                    <a href="/">
-                        Bellissima
-                    </a>
-                </h1>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className="holder">
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Agbalumo&family=Open+Sans:wght@300;400;700&display=swap"
+        rel="stylesheet"
+      ></link>
+      <div className="descuento">
+        <p>¡Envío gratis en compras a partir de $15.000!</p>
+        <FontAwesomeIcon
+          icon={faSackDollar}
+          bounce
+          size="2xl"
+          style={{ color: "#16ca43" }}
+        />
+      </div>
+      <div className="boton">
+        <button type="submit" className="btn btn-outline-danger btn-lg">
+          <a
+            href="http://localhost:3000/admin/novedades"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h3>
+              Ingresar <i class="fa-solid fa-right-to-bracket"></i>
+            </h3>
+          </a>
+        </button>
+      </div>
+      <div className="logo">
+        <img src="img/logo.png" alt="logo" />
+        <h1>
+          <Link href="/">Bellissima</Link>
+        </h1>
+      </div>
+    </header>
+  );
+};
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,62 +1,94 @@
 import React from "react";
-
-import Dropdown from 'react-bootstrap/Dropdown';
-
-import '../styles/Nav.css'
+import Dropdown from "react-bootstrap/Dropdown";
+import "../styles/Nav.css";
 import { Link } from "react-router-dom";
 const Nav = (props) => {
-    return (
-        <div className='Nav holder'>
-            <ul>
-                <li>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Otoño-Invierno
-                        </Dropdown.Toggle>
+  return (
+    <div className="Nav holder">
+      <ul>
+        <li>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Otoño-Invierno
+            </Dropdown.Toggle>
 
-                        <Dropdown.Menu className='lista'>
-                            <Dropdown.Item href="/abrigos">Abrigos</Dropdown.Item>
-                            <Dropdown.Item href="/gorro">Gorros/Bufandas</Dropdown.Item>
-                            <Dropdown.Item href="/pantalon">Pantalon</Dropdown.Item>
-                            <Dropdown.Item href="/pollera">Polleras</Dropdown.Item>
-                            <Dropdown.Item href="/remeras">Remeras</Dropdown.Item>
-                            <Dropdown.Item href="/sweater">Sweater</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </li>
-                <li>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Primavera-Verano
-                        </Dropdown.Toggle>
+            <Dropdown.Menu className="lista">
+              <Dropdown.Item as={Link} to="/abrigos">
+                Abrigos
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/gorro">
+                Gorros/Bufandas
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/pantalon">
+                Pantalon
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/pollera">
+                Polleras
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/remeras">
+                Remeras
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/sweater">
+                Sweater
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </li>
+        <li>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Primavera-Verano
+            </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="/blusas">Blusas/Remeras</Dropdown.Item>
-                            <Dropdown.Item href="/camisas">Camisas</Dropdown.Item>
-                            <Dropdown.Item href="/gorros">Gorros</Dropdown.Item>
-                            <Dropdown.Item href="/pantalonpri">Pantalon</Dropdown.Item>
-                            <Dropdown.Item href="/polleras">Polleras</Dropdown.Item>
-                            <Dropdown.Item href="/vestidos">Vestidos</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </li>
-                <li>
-                    <Link to="/outlet" class="link">Outlet</Link>
-                </li>
-                <li>
-                    <Link to="/accesorios" class="link">Accesorios</Link>
-                </li>
-                <li>
-                    <Link to="/supersale" class="link">¡Super-Sale!</Link>
-                </li>
-                <li>
-                    <Link to="/novedades" class="link">¡Novedades!</Link>
-                </li>
-                <li>
-                    <Link to="/contacto" class="link">¡Contactanos!</Link>
-                </li>
-            </ul>
-        </div>
-    );
-}
+            <Dropdown.Menu>
+              <Dropdown.Item as={Link} to="/blusas">
+                Blusas/Remeras
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/camisas">
+                Camisas
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/gorros">
+                Gorros
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/pantalonpri">
+                Pantalon
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/polleras">
+                Polleras
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/vestidos">
+                Vestidos
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </li>
+        <li>
+          <Link to="/outlet" class="link">
+            Outlet
+          </Link>
+        </li>
+        <li>
+          <Link to="/accesorios" class="link">
+            Accesorios
+          </Link>
+        </li>
+        <li>
+          <Link to="/supersale" class="link">
+            ¡Super-Sale!
+          </Link>
+        </li>
+        <li>
+          <Link to="/novedades" class="link">
+            ¡Novedades!
+          </Link>
+        </li>
+        <li>
+          <Link to="/contacto" class="link">
+            ¡Contactanos!
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 export default Nav;
